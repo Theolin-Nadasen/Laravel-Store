2c5583d524d31975b46019073bdfa42ea7fb689b
Theolin Nadasen <nadasentheolin@gmail.com>
2025-03-21 20:14:46 +0200

    Landing page updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,19 +9,23 @@
 
     @foreach ($products as $product)
 
-    <div class="container-fluid">
+    <div class="container-fluid  bg-light shadow mb-2">
 
     
-    <div class="row mt-5">
+    <div class="row mt-5 mb-5">
+        <div class="col-2"></div>
+
         <div class="col-10 col-md-4">
             <img src="{{asset('storage/' . $product->image)}}" alt="Product Image" width="256">
         </div>
 
-        <div class="col-10 col-md-6">
+        <div class="col-2 d-md-none"></div>
+
+        <div class="col-10 col-md-6 d-flex flex-column justify-content-between">
             <p>{{$product->description}}</p>
 
             @if(Auth::user())
-            <a href="{{route('addtocart', ['product' => $product])}}" class="btn btn-info">Add To Cart (R{{$product->price}})</a>
+            <a href="{{route('addtocart', ['product' => $product])}}" class="btn btn-info w-50">Add To Cart (R{{$product->price}})</a>
             @endif
         </div>
     </div>
